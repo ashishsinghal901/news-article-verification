ce2cb439e45d5214cbaa79fb231683714dc13b3e
Ashish Singhal <99002173+ashishdiscovery@users.noreply.github.com>
2022-07-27 19:13:24 +0530

    removed target folder

--- target/test-output-thread/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"1e927a93-48f5-4e9e-95d7-c6b8c220960e","feature":"Guardian News Test","scenario":"As a user I validate first news on the Guardian home page is valid","start":1658926257089,"group":21,"content":"","tags":"@gui,","end":1658926266687,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":21,"content":"Thread[TestNG-PoolService-0,5,main]"}]);
-});
